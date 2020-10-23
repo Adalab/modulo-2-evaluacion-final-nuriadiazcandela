286bc8bb9c8158d6c461948e51f0200ebec3370d
nuriadiazcandela <nuriadiazcandela@gmail.com>
2020-10-23 20:13:13 +0200

    add favourite list

--- js/main.js
+++ js/main.js
@@ -3,13 +3,14 @@
 const box = document.querySelector('.js-text');
 const btn = document.querySelector('.js-btn');
 const series = document.querySelector('.js-result');
-const seriesFavourites = document.querySelector('.js-favourite');
+const addFavourites = document.querySelector('.js-favourite');
 
 let seriesResult = [];
 let seriesFavourite = [];
 
 // FUNCION PARA OBTENER DATOS DE LA API
-function getSerie() {
+function getSerie(ev) {
+  ev.preventDefault();
   const serie = box.value;
   fetch(`http://api.tvmaze.com/search/shows?q=${serie}`)
     .then((response) => response.json())
@@ -23,21 +24,27 @@ function getSerie() {
 // FUNCION PARA PINTAR LOS RESULTADOS EN HTML
 function paintSerie() {
   for (let i = 0; i < seriesResult.length; i++) {
-    // si se encuentra dentro de nuestro array de favoritos, se pinta, si no, se queda vacio
     let classFavourite;
     const favoClass = seriesFavourite.indexOf(i);
     const favourite = favoClass !== -1;
     if (favourite === true) {
-      classFavourite = 'color_favourite';
+      classFavourite = 'color-favourite js-fav-item';
     } else {
       classFavourite = '';
     }
-
-    series.innerHTML += `<li class = "js-serie-item ${classFavourite}" id=${i}><h3> ${seriesResult[i].show.name}  </h3>`;
-    series.innerHTML += `<div >`;
-    series.innerHTML += `<img src = ${seriesResult[i].show.image.medium} alt = picture show >`;
-    series.innerHTML += `</div>`;
-    series.innerHTML += `</li>`;
+    if (favourite) {
+      addFavourites.innerHTML += `<li class = "js-fav-item ${classFavourite}" id=${i}><h3 class ="title"> ${seriesResult[i].show.name}</h3>`;
+      addFavourites.innerHTML += `<div >`;
+      addFavourites.innerHTML += `<img src = ${seriesResult[i].show.image.medium} alt = picture show >`;
+      // addFavourites.innerHTML += `</div>`;
+      // addFavourites.innerHTML += `</li>`;
+    } else {
+      series.innerHTML += `<li class = "js-serie-item ${classFavourite}" id=${i}><h3 class ="title"> ${seriesResult[i].show.name}</h3>`;
+      series.innerHTML += `<div >`;
+      series.innerHTML += `<img src = ${seriesResult[i].show.image.medium} alt = picture show >`;
+      series.innerHTML += `</div>`;
+      series.innerHTML += `</li>`;
+    }
   }
 }
 
@@ -50,23 +57,32 @@ const favouriteSerie = function (event) {
 
   if (isFavourite === false) {
     seriesFavourite.push(clicked);
+
+    console.log(seriesFavourite);
     console.log('lo meto');
   } else {
     seriesFavourite.splice(indexFav, 1);
     console.log('lo quito');
   }
-
   paintSerie();
   listenSerie();
-  console.log(seriesFavourite);
 };
 
-//
+// FUNCION ESCUCHADORA SOBRE CADA UNA DE LAS SERIES DEL RESULTADO DE LA BUSQUEDA
 function listenSerie() {
   const seriesItems = document.querySelectorAll('.js-serie-item');
   for (const seriesItem of seriesItems) {
     seriesItem.addEventListener('click', favouriteSerie);
   }
 }
+// FUNCION ESCUCHADORA SOBRE SERIE FAVORITA
+function listenFavs() {
+  const favItems = document.querySelectorAll('.js-fav-item');
+  for (const favItem of favItems) {
+    favItem.addEventListener('click', favouriteSerie);
+  }
+}
 
 btn.addEventListener('click', getSerie);
+
+paintSerie();
